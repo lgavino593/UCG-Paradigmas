7dfddcddf3d96376bef7c8139d942f3c088387d2
lgavino593 <luis.armando.gavino.armijos@gmail.com>
2026-09-19 11:34:58 -0500

    Fix annual rate input value in app (1).py

--- app (1).py
+++ app (1).py
@@ -8,7 +8,7 @@ st.sidebar.image("logo.jpeg")
 st.sidebar.title("Parametros")
 
 capital = st.number_input("Ingrese el capital", value=1000)
-tasa_anual  = st.number_input("Ingrese la tasa anual", value =0-15)
+tasa_anual  = st.number_input("Ingrese la tasa anual", value =0.15)
 dias_mora  = st.number_input("Ingrese dias", value= 30)
 
 resultado = lf.calcular_interes_mora(capital,tasa_anual, dias_mora)
