7ab0ef8adc70a5c00a4e65ef8a6ff7e8004dbf42
lgavino593 <luis.armando.gavino.armijos@gmail.com>
2026-09-19 11:32:54 -0500

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -7,9 +7,10 @@ st.sidebar.image("logo.jpeg")
 
 st.sidebar.title("Parametros")
 
-capital = st.number_input("Ingrese el capital")
-tasa_anual  = st.number_input("Ingrese la tasa anual")
-dias_mora  = st.number_input("Ingrese dias")
+capital = st.number_input("Ingrese el capital", value=1000)
+tasa_anual  = st.number_input("Ingrese la tasa anual", value =0-15)
+dias_mora  = st.number_input("Ingrese dias", value= 30)
 
+resultado = lf.calcular_interes_mora(capital,tasa_anual, dias_mora)
 
 st.write("Elaborado Por: Luis Gavino ")
