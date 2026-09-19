f98090556dc8889a064dc79fa8139827a8737ec3
lgavino593 <luis.armando.gavino.armijos@gmail.com>
2026-09-19 11:28:49 -0500

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -1,9 +1,12 @@
 import streamlit as st
+import libreria_funciones as lf
 
 st.title("Paradigma de la Programacion")
 
 st.sidebar.image("logo.jpeg")
 
 st.sidebar.title("Parametros")
+
+capital = st.number_imput("Ingrese el capital")
 
 st.write("Elaborado Por: Luis Gavino ")
